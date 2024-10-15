b5e117b3fb4a9f688f1c5eb0d944e5a7f291af7a
hehaifuture <2601097010@qq.com>
2024-10-15 17:50:27 +0800

    Update ModItems.java

--- src/GalaxyTimes/ModItems.java
+++ src/GalaxyTimes/ModItems.java
@@ -1,18 +1,10 @@
 package GalaxyTimes;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.type.Item;
 
 public class ModItems {
-  public static Item ice;
-  public static void load() {
-    ice = new Item("ice");
-  }
-  
+    public static Item ice;
+    public static void load() {
+        ice = new Item("ice");
+    }
 }
